3066593a9314172730945cef318359e3e98f1b09
joacogithub <133706597+joacogithub@users.noreply.github.com>
2023-09-03 18:47:54 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("autos").addEventListener("click", function() {
+        localStorage.setItem("catID", 101);
+        window.location = "products.html"
+    });
+    document.getElementById("juguetes").addEventListener("click", function() {
+        localStorage.setItem("catID", 102);
+        window.location = "products.html"
+    });
+    document.getElementById("muebles").addEventListener("click", function() {
+        localStorage.setItem("catID", 103);
+        window.location = "products.html"
+    });
+
+
+
+
+    let usuario = localStorage.getItem('nombre');
+    if (usuario=="" || usuario==null){
+        location.href='login.html';
+    }else{
+        document.getElementById('nombre').innerHTML= usuario;
+    }
+
+    let logout = document.getElementById('salir');
+    logout.addEventListener('click', function(){
+        localStorage.removeItem('nombre');
+        alert('Desconexion exitosa', 'Vuelve pronto');
+        location.href="login.html";
+        
+    })
+});
+
